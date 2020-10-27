ec7000d7f9b7fc6679d9f229f85d892fac40371c
0prahWinfrey <56882532+0prahWinfrey@users.noreply.github.com>
2020-10-26 20:48:46 -0600

    Update irisGuessByPetalWidth.sql

--- SQL/Fisher/irisGuessByPetalWidth.sql
+++ SQL/Fisher/irisGuessByPetalWidth.sql
@@ -1,3 +1,7 @@
+-- drop aGuess Column
+ALTER TABLE iris
+DROP aGuess;
+
 -- this column will hold iris guesses
 ALTER TABLE iris
 ADD aGuess varchar(50);
@@ -10,9 +14,14 @@ WHERE iris.petalLength < 2.5;
 -- Versicolor
 UPDATE iris
 SET aGuess = 'versicolor'
-WHERE iris.petalwidth > 1.6 AND iris.aGuess IS NULL;
+WHERE iris.petalwidth < 1.7 AND iris.aGuess IS NULL;
 
 -- Virginica
 UPDATE iris
 SET aGuess = 'virginica'
 WHERE iris.aGuess IS NULL;
+
+-- How many flowers missattributed 
+SELECT COUNT(*) 
+FROM iris
+WHERE iris.species <> iris.aguess;
